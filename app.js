e0f2d42642f58ec05f213bb522901c6234327a41
Bisworanjan34 <bisworanjan34@gmail.com>
2025-06-07 16:41:47 +0530

    4th commit

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 let corse= require('cors');
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 const router = require('./route');
-mongoose.connect('mongodb://localhost:27017/mern', {
-    useNewUrlParser: true,  
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+const port = process.env.PORT || 3000;
+const mongoUrl = process.env.mongo_url;
+
+mongoose.connect(mongoUrl)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 app.use(corse());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use('/auth',router); // Adjust the path as necessary
-
-app.listen(2030, () => {
-    console.log("Server is running on port 2030");
-});
